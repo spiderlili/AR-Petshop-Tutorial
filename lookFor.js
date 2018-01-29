2229369652d4eb288b7ee4dba344465075a67561
Jing Chun Tan <jtan1993@gmail.com>
2018-01-29 16:19:42 +0000

    Update lookFor.js

--- lookFor.js
+++ lookFor.js
@@ -1,3 +1,11 @@
+/*
+This sub-symbol is a little prompt that appears on the left hand side of the screen and directs the user to look at the target image.
+
+1. Drag and drop it into your symbol definitions
+2. Drag and drop it into your hierarchy at the end.
+3. In the references of the Look For, change the target_image to be a small thumbnail jpg of the poster.
+*/
+
 Z.node("root").on("ready",function(){
 	Z.controller("X:delay").timeline("delay").time(0);
 	Z.controller("X:delay").timeline("delay").play();
