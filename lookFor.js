50b7d1db346f560b295c84321eaceee2ed957db6
Jing Chun Tan <jtan1993@gmail.com>
2018-01-29 15:59:01 +0000

    Create lookFor.js

---
+++ lookFor.js
@@ -0,0 +1,20 @@
+Z.node("root").on("ready",function(){
+	Z.controller("X:delay").timeline("delay").time(0);
+	Z.controller("X:delay").timeline("delay").play();
+});
+
+Z.controller("X:delay").timeline("delay").on("complete",function(){
+		Z.controller("X:loop").timeline("loop").play();
+	  Z.controller("main").state("active").activate();
+	});
+
+export function activate(){
+  Z.controller("X:delay").timeline("delay").time(0);
+	Z.controller("X:delay").timeline("delay").play();
+};
+
+export function deactivate(){
+	Z.controller("X:loop").timeline("loop").stop();
+	Z.controller("X:delay").timeline("delay").stop();
+	Z.controller("main").state("inactive").activate();
+};
